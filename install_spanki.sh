2e37f75e850096b6908c49571f6e808034c38783
Fu Xing <bio.xfu@gmail.com>
2018-09-03 14:12:10 +0800

    update

--- install_spanki.sh
+++ install_spanki.sh
@@ -1,3 +1,6 @@
-conda create -y -n spanki parallel samtools=0.1.19
+conda create -y -n spanki pip parallel samtools=0.1.19 python=2.7
+source activate spanki
+pip install numpy
+
 cd Spanki_0.5.0_xfu
 python setup.py install
